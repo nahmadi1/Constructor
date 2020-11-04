82ae84083e06036d846835d7e36193338dd459ac
Nasrat Ahmadi <nsahdat@yahoo.com>
2020-11-04 10:45:13 -0500

    MyJavaConstructor

---
+++ Constructor/src/constructorInJava/ConstractorDemo.java
@@ -0,0 +1,10 @@
+package constructorInJava;
+
+public class ConstractorDemo {
+
+	
+	public void submit() {
+		
+		System.out.println("User cliks submit button");
+	}
+}
---
+++ Constructor/src/constructorInJava/ConstractorDemoTest.java
@@ -0,0 +1,13 @@
+package constructorInJava;
+
+public class ConstractorDemoTest {
+
+	public static void main(String[] args) {
+	
+		
+		ConstractorDemo obj = new ConstractorDemo();
+		obj.submit();
+
+	}
+
+}
